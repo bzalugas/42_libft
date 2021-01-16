e3ba5a3cfa399e133e8a891158989a89ac2cf82e
Bastien Zalugas <bastien.zalugas@hotmail.fr>
2021-01-16 03:03:48 +0100

    modified isalpha (added ft_islower & ft_isupper)

--- ft_isalpha.c
+++ ft_isalpha.c
@@ -6,25 +6,11 @@
 /*   By: bzalugas <bzalugas@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/01/16 02:02:13 by bzalugas          #+#    #+#             */
-/*   Updated: 2021/01/16 02:22:17 by bzalugas         ###   ########.fr       */
+/*   Updated: 2021/01/16 02:46:36 by bzalugas         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
-
-static int	ft_islower(int c)
-{
-	if (c >= 'a' && c <= 'z')
-		return (1);
-	return (0);
-}
-
-static int	ft_isupper(int c)
-{
-	if (c >= 'A' && c <= 'Z')
-		return (1);
-	return (0);
-}
 
 int			ft_isalpha(int c)
 {

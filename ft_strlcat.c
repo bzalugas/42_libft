dca163a7c1c49efcdb729008b708353b9fb4b900
Bastien Zalugas <bastien.zalugas@hotmail.fr>
2021-01-18 23:44:08 +0100

    Added src_len to use ft_strlen just one time

--- ft_strlcat.c
+++ ft_strlcat.c
@@ -6,7 +6,7 @@
 /*   By: bzalugas <bzalugas@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/01/16 04:17:27 by bzalugas          #+#    #+#             */
-/*   Updated: 2021/01/18 22:09:02 by bzalugas         ###   ########.fr       */
+/*   Updated: 2021/01/18 23:33:27 by bzalugas         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,19 +15,21 @@
 size_t	ft_strlcat(char *dst, const char *src, size_t dstsize)
 {
 	size_t	i;
-	size_t	len;
+	size_t	dst_len;
+	size_t	src_len;
 
-	len = ft_strlen(dst);
-	if (len < dstsize && dstsize > 0)
+	dst_len = ft_strlen(dst);
+	src_len = ft_strlen(src);
+	if (dst_len < dstsize && dstsize > 0)
 	{
 		i = 0;
-		while (src[i] && i < dstsize - len - 1)
+		while (src[i] && i < dstsize - dst_len - 1)
 		{
-			dst[len + i] = src[i];
+			dst[dst_len + i] = src[i];
 			i++;
 		}
-		dst[len + i] = '\0';
-		return (len + ft_strlen(src));
+		dst[dst_len + i] = '\0';
+		return (dst_len + src_len);
 	}
-	return (dstsize + ft_strlen(src));
+	return (dstsize + src_len);
 }

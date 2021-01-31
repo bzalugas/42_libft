15b0d44ba6f1dd3e67184cc96a8c37932d2ba6b8
Bastien <bastien.zalugas@student.42.fr>
2021-01-31 19:58:09 +0100

    Update strnstr after a moulinette error

--- ft_strnstr.c
+++ ft_strnstr.c
@@ -6,7 +6,7 @@
 /*   By: bzalugas <bzalugas@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/01/18 21:20:16 by bzalugas          #+#    #+#             */
-/*   Updated: 2021/01/18 22:09:35 by bzalugas         ###   ########.fr       */
+/*   Updated: 2021/01/31 19:48:02 by bzalugas         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,21 +17,18 @@ char	*ft_strnstr(const char *haystack, const char *needle, size_t len)
 	size_t i;
 	size_t j;
 
-	if (needle[0] != '\0')
+	if (!needle)
+		return ((char *)haystack);
+	i = 0;
+	j = 0;
+	while (haystack[i + j] && i + j < len)
 	{
-		i = 0;
-		j = 0;
-		while (haystack[i] && i < len)
-		{
-			if (needle[j] && haystack[i] == needle[j])
-				j++;
-			else
-				j = 0;
-			if (needle[j] == '\0')
-				return ((char *)&haystack[i - j + 1]);
-			i++;
-		}
-		return (NULL);
+		if (needle[j] && needle[j] == haystack[i + j])
+			j++;
+		else if (!needle[j])
+			return ((char *)&haystack[i]);
+		else if (++i)
+			j = 0;
 	}
-	return ((char *)haystack);
+	return (NULL);
 }

ea6410da1752609dd846ee37ed8be3faabbc527c
Bastien Zalugas <bastien.zalugas@hotmail.fr>
2021-02-01 11:07:48 +0100

    Update strnstr (hope it works)

--- ft_strnstr.c
+++ ft_strnstr.c
@@ -6,7 +6,7 @@
 /*   By: bzalugas <bzalugas@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/01/18 21:20:16 by bzalugas          #+#    #+#             */
-/*   Updated: 2021/01/31 19:48:02 by bzalugas         ###   ########.fr       */
+/*   Updated: 2021/02/01 10:54:21 by bzalugas         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,11 +17,11 @@ char	*ft_strnstr(const char *haystack, const char *needle, size_t len)
 	size_t i;
 	size_t j;
 
-	if (!needle)
+	if (!*needle)
 		return ((char *)haystack);
 	i = 0;
 	j = 0;
-	while (haystack[i + j] && i + j < len)
+	while (haystack[i] && i + j <= len)
 	{
 		if (needle[j] && needle[j] == haystack[i + j])
 			j++;

76adf9ae1239f788e32c7a5e889395105a81cb14
Bastien <bastien.zalugas@student.42.fr>
2023-11-13 16:16:14 +0100

    Add ft_memchr

--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: bazaluga <bazaluga@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/04 17:22:49 by bazaluga          #+#    #+#             */
-/*   Updated: 2023/11/13 14:48:16 by bazaluga         ###   ########.fr       */
+/*   Updated: 2023/11/13 15:16:11 by bazaluga         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -32,5 +32,6 @@ int		ft_tolower(int c);
 char	*ft_strchr(const char *s, int c);
 char	*ft_strrchr(const char *s, int c);
 int		ft_strncmp(const char *s1, const char *s2, size_t n);
+void	*ft_memchr(const void *s, int c, size_t n);
 
 #endif

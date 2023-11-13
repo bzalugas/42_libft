116824ea13664ee6bc7dd4b01a70aa31d7d3e53e
Bastien <bastien.zalugas@student.42.fr>
2023-11-13 15:15:05 +0100

    Add strncmp

--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: bazaluga <bazaluga@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/04 17:22:49 by bazaluga          #+#    #+#             */
-/*   Updated: 2023/11/13 12:02:30 by bazaluga         ###   ########.fr       */
+/*   Updated: 2023/11/13 14:48:16 by bazaluga         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -31,5 +31,6 @@ int		ft_toupper(int c);
 int		ft_tolower(int c);
 char	*ft_strchr(const char *s, int c);
 char	*ft_strrchr(const char *s, int c);
+int		ft_strncmp(const char *s1, const char *s2, size_t n);
 
 #endif

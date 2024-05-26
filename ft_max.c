60bfee344c5ed733e78eded1dea3c13f03a87df4
bazaluga <bazaluga@student.42.fr>
2024-05-26 19:11:21 +0200

    refactor: finish update

--- ft_max.c
+++ ft_max.c
@@ -6,7 +6,7 @@
 /*   By: bazaluga <bazaluga@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/12 14:49:36 by bazaluga          #+#    #+#             */
-/*   Updated: 2024/05/06 23:02:07 by bazaluga         ###   ########.fr       */
+/*   Updated: 2024/05/26 19:08:37 by bazaluga         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,22 +17,4 @@ int	ft_max(int a, int b)
 	if (a > b)
 		return (a);
 	return (b);
-}
-
-int	*ft_max_arr(int *arr, size_t len)
-{
-	size_t	i;
-	int		*max;
-
-	if (!arr)
-		return ((void *)0);
-	max = &arr[0];
-	i = 1;
-	while (i < len)
-	{
-		if (arr[i] > *max)
-			max = &arr[i];
-		i++;
-	}
-	return (max);
 }
--- ft_max_arr.c
+++ ft_max_arr.c
@@ -1,20 +1,31 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_min.c                                           :+:      :+:    :+:   */
+/*   ft_max_arr.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: bazaluga <bazaluga@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/03/12 14:51:43 by bazaluga          #+#    #+#             */
-/*   Updated: 2024/05/26 19:09:06 by bazaluga         ###   ########.fr       */
+/*   Created: 2024/05/26 19:08:20 by bazaluga          #+#    #+#             */
+/*   Updated: 2024/05/26 19:08:27 by bazaluga         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <sys/types.h>
 
-int	ft_min(int a, int b)
+int	*ft_max_arr(int *arr, size_t len)
 {
-	if (a < b)
-		return (a);
-	return (b);
+	size_t	i;
+	int		*max;
+
+	if (!arr)
+		return ((void *)0);
+	max = &arr[0];
+	i = 1;
+	while (i < len)
+	{
+		if (arr[i] > *max)
+			max = &arr[i];
+		i++;
+	}
+	return (max);
 }

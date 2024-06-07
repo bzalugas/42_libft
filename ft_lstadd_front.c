8868d077b3b1defa15e4d40a978b8639da4ac0f8
bazaluga <bazaluga@student.42.fr>
2024-06-07 12:39:51 +0200

    feat: better lstadd_back & lstadd_front

--- ft_lstadd_front.c
+++ ft_lstadd_front.c
@@ -6,20 +6,21 @@
 /*   By: bazaluga <bazaluga@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/09 14:04:08 by bazaluga          #+#    #+#             */
-/*   Updated: 2023/12/09 16:41:02 by bazaluga         ###   ########.fr       */
+/*   Updated: 2024/06/07 12:39:16 by bazaluga         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-void	ft_lstadd_front(t_list **lst, t_list *new)
+void	*ft_lstadd_front(t_list **lst, t_list *new)
 {
 	t_list	*old;
 
 	if (!lst || !new)
-		return ;
+		return (NULL);
 	old = *lst;
 	*lst = new;
 	if (new)
 		new->next = old;
+	return (new);
 }
--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: bazaluga <bazaluga@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/04 17:22:49 by bazaluga          #+#    #+#             */
-/*   Updated: 2024/06/07 12:32:45 by bazaluga         ###   ########.fr       */
+/*   Updated: 2024/06/07 12:39:01 by bazaluga         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -64,10 +64,10 @@ typedef struct s_list
 }				t_list;
 
 t_list			*ft_lstnew(void *content);
-void			ft_lstadd_front(t_list **lst, t_list *new);
+void			*ft_lstadd_front(t_list **lst, t_list *new);
 int				ft_lstsize(t_list *lst);
 t_list			*ft_lstlast(t_list *lst);
-void			ft_lstadd_back(t_list **lst, t_list *new);
+void			*ft_lstadd_back(t_list **lst, t_list *new);
 void			ft_lstdelone(t_list *lst, void (*del)(void *));
 void			ft_lstclear(t_list **lst, void (*del)(void *));
 void			ft_lstiter(t_list *lst, void (*f)(void *));
